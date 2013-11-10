a025a87fe9a4d40df57c4f6b6e523cce486a408d
tomoemon <bach48+github_tomoemon@gmail.com>
2013-11-10 23:41:40 +0900

    first commit

---
+++ Standby/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Standby
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PowerState state = PowerState.Suspend;
+            bool force = false;
+            bool disableWakeEvent = false;
+
+            foreach (string arg in Environment.GetCommandLineArgs())
+            {
+                switch (arg)
+                {
+                    case "--force":
+                        force = true;
+                        break;
+                    case "--disableWakeEvent":
+                        disableWakeEvent = true;
+                        break;
+                    case "--hibernate":
+                        state = PowerState.Hibernate;
+                        break;
+                }
+            }
+            Application.SetSuspendState(state, force, disableWakeEvent);
+        }
+    }
+}
